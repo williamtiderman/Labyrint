41752e7881986b7cec87f5af7b643afe5eae85cb
John <engblomsandinjohn@gmail.com>
2021-01-10 20:36:33 +0100

    imports

--- Labyrint/src/mazeGeneration/Main.java
+++ Labyrint/src/mazeGeneration/Main.java
@@ -5,47 +5,58 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		MazeGeneration maze = null;
+		MazeSolver mazeSolver = null;
+
 		Scanner choose = new Scanner(System.in);
 		System.out.println("Vilken labyrint generator vill du ha? "
 				+ "Rekursiv eller Sidewind, R för Rekursiv och S för Sidewind. Annars skriv I om du vill importa från textfil");
 		String yesNo = choose.nextLine();
-		
-		
-		
-		System.out.println("Hur stor ska labyrinten vara(Skriv en sida för kvadranten)");
-		String size = choose.nextLine();
-		
-		MazeGeneration maze = null;
-		MazeSolver mazeSolver = null;
-		
-		
-		int n = 15;
-		try {
-			n = Integer.parseInt(size);
-		} catch (NumberFormatException e) {
-			System.out.println("Inte ett nummer");
-			e.printStackTrace();
+
+		if(yesNo.equalsIgnoreCase("I")) {
+			System.out.println("Skriv in den exakt sökvägen till textfilen");
+			String filePath = choose.nextLine();
+
+			maze = new MazeImport(filePath);
 		}
-		
-		if(yesNo.equalsIgnoreCase("R")) {
-			maze = new MazeGenRecBack(n);
-			StdDraw.enableDoubleBuffering();
+		else {
+			System.out.println("Hur stor ska labyrinten vara(Skriv en sida för kvadranten)");
+			String size = choose.nextLine();
+			
+			int n = 15;
+			try {
+				n = Integer.parseInt(size);
+			} catch (NumberFormatException e) {
+				System.out.println("Inte ett nummer");
+				e.printStackTrace();
+			}
+
+			if(yesNo.equalsIgnoreCase("R")) {
+				maze = new MazeGenRecBack(n);
+				StdDraw.enableDoubleBuffering();
+			}
+			else if(yesNo.equalsIgnoreCase("S")) {
+				maze = new MazeGenSideWind(n);
+				StdDraw.enableDoubleBuffering();
+			}
+			else if(!yesNo.equalsIgnoreCase("r") || !yesNo.equalsIgnoreCase("s")){
+				System.out.println("Fel inmatning av val");
+			}
 		}
-		else if(yesNo.equalsIgnoreCase("S")) {
-			maze = new MazeGenSideWind(n);
-			StdDraw.enableDoubleBuffering();
-		}
-		else if(!yesNo.equalsIgnoreCase("r") || !yesNo.equalsIgnoreCase("s")){
-			System.out.println("Fel inmatning av val");
-		}
-		
+
+
+
+
+
+
+
 		System.out.println("Labyrint klar, ska den lösas eller printas till textfil (L eller P)?");
 		String solveOrPrint = choose.nextLine();
-		
+
 		if(solveOrPrint.equalsIgnoreCase("L")) {
 			System.out.println("Vad för algoritm, depth-first eller stack loop (D eller S)?");
 			String solve = choose.nextLine();
-			
+
 			if(solve.equalsIgnoreCase("D")) {
 				mazeSolver = new MazeSolveDepthFirst(maze);
 			}
@@ -53,7 +64,7 @@ public class Main {
 				mazeSolver = new MazeSolveLoop(maze);
 			}
 			mazeSolver.solve();
-			
+
 		}
 		else if(solveOrPrint.equalsIgnoreCase("P")) {
 			MazeExport export = new MazeExport(maze);
@@ -62,7 +73,7 @@ public class Main {
 		else if(!solveOrPrint.equalsIgnoreCase("P") || !solveOrPrint.equalsIgnoreCase("L")){
 			System.out.println("Fel inmatning av val");
 		}
-		
+
 
 		choose.close();
 	}
--- Labyrint/src/mazeGeneration/MazeImport.java
+++ Labyrint/src/mazeGeneration/MazeImport.java
@@ -6,17 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MazeImport {
-	
+public class MazeImport implements MazeGeneration{
+
 	private int n;
 	private List<List<Boolean>> north;
 	private List<List<Boolean>> east;
 	private List<List<Boolean>> west;
 	private List<List<Boolean>> south;
 	private List<List<Boolean>> visited;
+	MazeGeneration maze = null;
 
-	public MazeGeneration MazeImport(String filePath) {
-		
+	public MazeImport(String filePath) {
+		maze = importMaze(filePath);
+	}
+
+	public MazeGeneration importMaze(String filePath) {
 		String data = "";
 
 		north = new ArrayList<List<Boolean>>();
@@ -24,10 +28,10 @@ public class MazeImport {
 		west = new ArrayList<List<Boolean>>();
 		south = new ArrayList<List<Boolean>>();
 		visited = new ArrayList<List<Boolean>>();
-		
-		MazeGeneration maze = null;
-		
-		
+
+
+
+
 		File textFile = new File(filePath);
 		Scanner mazeFile = null;
 		try {
@@ -36,7 +40,7 @@ public class MazeImport {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		while (mazeFile.hasNextLine()) 
 		{
 			data += mazeFile.nextLine();
@@ -46,15 +50,15 @@ public class MazeImport {
 		Scanner dataReader = new Scanner(data);
 		dataReader.useDelimiter(" |\n");
 
-		
+
 		boolean firstRun = true;
-		
+
 		while(dataReader.hasNext())
 		{
 			if(firstRun) {
 				firstRun = false;
 				dataReader.nextLine();
-				
+
 				n = Integer.parseInt(dataReader.next());
 				for(int i = 0; i < n+2; i++){
 					north.add(new ArrayList<Boolean>());
@@ -71,7 +75,7 @@ public class MazeImport {
 						visited.get(i).add(false);
 					}
 				}
-				 maze = new ImportedMaze(north,east,south,west,visited,n);
+				maze = new ImportedMaze(north,east,south,west,visited,n);
 			}
 			else{
 				int x = Integer.parseInt(dataReader.next());
@@ -80,13 +84,86 @@ public class MazeImport {
 				maze.getEast().get(x).set(y,Boolean.parseBoolean(dataReader.next()));
 				maze.getSouth().get(x).set(y,Boolean.parseBoolean(dataReader.next()));
 				maze.getWest().get(x).set(y,Boolean.parseBoolean(dataReader.next()));
+				
+				String systemPrint = x + "," + y + " " + north.get(x).get(y) + " " + east.get(x).get(y) + " " + south.get(x).get(y) + " " + west.get(x).get(y) + "\n";
+				System.out.println(systemPrint);
 			}
-			
+
 		}
-		
-		
+		draw();
+		dataReader.close();
 		return maze;
 	}
-	
-	
+
+	@Override
+	public void draw() {
+		StdDraw.clear();
+		StdDraw.pause(10);
+
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(n + 0.5, n + 0.5, 0.375);
+		StdDraw.filledCircle(1.5, 1.5, 0.375);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		for (int x = 1; x <= n; x++) {
+			for (int y = 1; y <= n; y++) {
+				if (south.get(x).get(y)) StdDraw.line(x, y, x+1, y);
+				if (north.get(x).get(y)) StdDraw.line(x, y+1, x+1, y+1);
+				if (west.get(x).get(y))  StdDraw.line(x, y, x, y+1);
+				if (east.get(x).get(y))  StdDraw.line(x+1, y, x+1, y+1);
+			}
+		}
+
+		StdDraw.show();
+
+	}
+
+	@Override
+	public List<List<Boolean>> getNorth() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<List<Boolean>> getEast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<List<Boolean>> getWest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<List<Boolean>> getSouth() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getN() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void generate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void generate(int x, int y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<List<Boolean>> getVisited() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 }
